984ce2070d702f9c491b831e2b8cb087aa68510b
Egor <Egor@DESKTOP-QGB3P2I>
2020-06-29 16:27:19 +0100

    finished

--- Main.js
+++ Main.js
@@ -8,16 +8,38 @@ var visited_nodes = [];
 var time_elapsed = 0;
 var timer;
 var flags_placed = 0;
+var resumes_timer = false;
 
 function display_help_modal()
 {
-    $('#help_icon').removeClass("blue-pulse");
+    clearInterval(timer);
     $('.modall-content').empty();
+    $('.modall-content').css({
+        "display" : "flex",
+        "align-items" : "center",
+        "font-size" : "1.5rem", 
+        "margin-top" : "15vh",
+        "flex-direction" : "column",
+        "max-width" : "800px"
+    })
     $('.modall-content').append(
         `
-
+            <p style = 'font-size: 2rem'>Minesweeper manual</p>
+            <p style = 'font-size: 1.1rem'>The aim of the game is to reveal all cells that don't have mines on them. If you reveal (click) on a cell that has a mine on it, you lose the game. If a revealed cell is adjacent to an unrevealed cell with a mine, that revealed cell will have a number that corresponds to a number of unrevealed cells with mines around that cell. See the picture below for practical example</p>
+            <img src='Assets/Example_1.png' style='width:65%; max-width:400px'>
+            <p style = 'font-size: 1.1rem; margin-top:10px;'>If you look at the the cell highlighted in blue, it has a number 3 written in it. This means that there are <strong>exactly</strong> 3 mines around that cell. Since there are only 3 unrevealed cells adjacent to the blue highlighted cell, that means that those 3 cells all have mines on them. </p>
+            <p style = 'font-size: 1.1rem;'>You can reveal a cell by left-clicking on that cell. Don't worry about clicking on a mine on your first click. The mines are only placed after the first click</p>
+            <img src='Assets/left-click-reveal.gif' style='width:65%; max-width:400px'>
+            <p style = 'font-size: 1.1rem; margin-top:10px;'>You can right-click on an unrevealed cell to place a flag on it. Flagged cells can't be clicked on, therefore if you suspect that this cell contains a mine, you can flag it and you won't accidentally click on it. You can unflag a cell by right-clicking it again. The number of flags that you can place is <strong>unlimited.</strong></p>
+            <img src='Assets/right-click.gif' style='width:65%; max-width:400px'>
+            <p style = 'font-size: 1.1rem; margin-top:10px;'>You can keep track of: total number of mines, flags placed and time elapsed on the interface above the playing filed</p>
+            <img src='Assets/game_overview.gif' style='width:65%; max-width:400px'>
         `
-    )
+    );
+    if(timer != null){
+        resumes_timer = true;
+    }
+    
     $('.modall').css("display","flex");
 }
 
@@ -26,37 +48,21 @@ $(function(){
         process_field_choice(this.id);
         
     });
-    $('#custom_field_submit').click(function(){
-        var x = $('#width_field').val();
-        var y = $('#height_field').val();
-        var mines = $('#mine_count').val();
+    
+    $('.close').click(function(){
         
-        var pattern = /^[1-9][0-9]?$/;
-        if(pattern.test(x) === true && pattern.test(y) === true){
-            var max_mines = Number(x) * Number(y) - 8;
-            if (/^[1-9][0-9]*$/.test(mines) == true){
-                if(Number(mines) <= max_mines){
-                    size_x = Number(x);
-                    size_y = Number(y);
-                    mine_count = Number(mines);
-                    $('.modall').remove();
-                    configure_and_display_field();
-                }
-                else{
-                    alert("Mine count is too big for the chosen field");
-                }
-            }
-            else{
-                alert("Fields can only contain positive integers");
-            }
+        $('.modall').hide();
+        if(resumes_timer === true)
+        {
+            timer = setInterval(update_time_elapsed, 1000);
+            resumes_timer = false;
         }
-        else{
-            alert("Fields can only contain positive integers less than 100");
-        }
-        
-        
-        
+    });
+    $('#help_icon').click(function(){
+        display_help_modal();
     })
+    
+    
     
 })
 function visited_nodes_includes(coord)
@@ -95,6 +101,69 @@ function process_field_choice(callerId)
 {
     
     if(callerId === "4"){
+        $('.modall-content').empty();
+        $('.modall-content').css({
+        "display" : "block",
+        "font-size" : "1rem",
+        "margin-top" : "20vh",
+        "max-width" : "500px"
+    })
+        $('.modall-content').append(
+        `
+            <form>
+                      <div class="form-group">
+                        
+                        <input type="text" class="form-control" id="width_field" placeholder="Width (max: 99)" >
+                        
+                      </div>
+                      <div class="form-group">
+                        
+                        <input type="text" class="form-control" id="height_field" placeholder="Height (max: 99)">
+                          
+                      </div>
+                      <div class="form-group">
+                          
+                        <input type="text" class="form-control" id="mine_count" placeholder="Mine count (max: width * height - 8)">
+                        
+                      </div>
+                      <div class="horizontalAlign">
+                          <button type="button" id="custom_field_submit" class="btn btn-primary" >Submit</button>
+                      </div>
+                      
+                </form>
+        `
+        );
+        $('#custom_field_submit').click(function(){
+        var x = $('#width_field').val();
+        var y = $('#height_field').val();
+        var mines = $('#mine_count').val();
+        
+        var pattern = /^[1-9][0-9]?$/;
+        if(pattern.test(x) === true && pattern.test(y) === true){
+            var max_mines = Number(x) * Number(y) - 8;
+            if (/^[1-9][0-9]*$/.test(mines) == true){
+                if(Number(mines) <= max_mines){
+                    size_x = Number(x);
+                    size_y = Number(y);
+                    mine_count = Number(mines);
+                    $('.modall').hide();
+                    configure_and_display_field();
+                }
+                else{
+                    alert("Mine count is too big for the chosen field");
+                }
+            }
+            else{
+                alert("Fields can only contain positive integers");
+            }
+        }
+        else{
+            alert("Fields can only contain positive integers less than 100");
+        }
+        
+        
+        
+        })
         $('.modall').css("display","flex")
     }
     else if(callerId == "1"){
@@ -153,7 +222,7 @@ function configure_and_display_field(){
         {
             $('#field_container').append(
             `
-            <img src="Assets/Empty_square3dd.png" id='${y}_${x}'>
+            <img src="Assets/unclicked_square.png" id='${y}_${x}'>
             `
             );
         }
@@ -164,8 +233,9 @@ function configure_and_display_field(){
         $('#' + this.id).attr("src","Assets/disabled_square.png")
         
     }, function(){
-        $('#' + this.id).attr("src","Assets/Empty_square3dd.png")
+        $('#' + this.id).attr("src","Assets/unclicked_square.png")
     })
+    
     $('#field_container img').click(function(){
         processClick(this.id);
     });
@@ -195,10 +265,10 @@ function configure_and_display_field(){
 
                 }, function(){
                 
-                $('#' + this.id).attr("src","Assets/Empty_square3dd.png")
+                $('#' + this.id).attr("src","Assets/unclicked_square.png")
                 
                 })
-            $(`#${this.id}`).attr("src", "Assets/Empty_square3dd.png");
+            $(`#${this.id}`).attr("src", "Assets/unclicked_square.png");
             flags_placed -= 1;
             
         }
@@ -285,6 +355,14 @@ function processClick (callerId)
             reveal_mines(y, x);
             
             $('#field_container img').off();
+            $('#alertMessageContainer').append(`
+                    <div class="alert alert-danger fade show" role="alert" id="bookmarkAlert" style="width:100%;margin:auto;margin-top:10px;max-width:600px;">
+                        You blew up on a mine! Game configuration: <strong>${size_x} x ${size_y}</strong> field with <strong>${mine_count}</strong> mines.<br> <strong>${time_elapsed}</strong> seconds elapsed since the start
+                    </div>
+
+                `);
+            $('#timer_and_flag_container').hide();
+            $('#alertMessageContainer').show();
             
         }
         else
@@ -295,9 +373,10 @@ function processClick (callerId)
             {
                 clearInterval(timer);
                 $('#field_container img').off();
+                reveal_mines(-1,-1);
                 $('#alertMessageContainer').append(`
                     <div class="alert alert-success fade show" role="alert" id="bookmarkAlert" style="width:100%;margin:auto;margin-top:10px;max-width:600px;">
-                        You have successfully minesweeped a <strong>${size_x} x ${size_y}</strong> field with <strong<${mine_count}</strong> mines. It took you <strong>${time_elapsed}</strong> seconds!
+                        You have successfully minesweeped a <strong>${size_x} x ${size_y}</strong> field with <strong>${mine_count}</strong> mines. <br>It took you <strong>${time_elapsed}</strong> seconds!
                     </div>
 
                 `);
@@ -408,8 +487,9 @@ function reveal_around(y, x)
     }
                 
 }
-function reveal_mines(y, x)
+function reveal_mines(y1, x1)
 {
+    
     for(var y = 0; y < size_y; y++)
     {
         for(var x = 0; x < size_x; x++)
@@ -420,6 +500,8 @@ function reveal_mines(y, x)
             }
         }
     }
+    console.log(y1, x1);
+    $(`#${y1}_${x1}`).attr("src","Assets/clicked_bomb.png");
 }
 
 function check_win_condition() // false - game still in process, true - game won 
